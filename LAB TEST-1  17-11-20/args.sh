6326a1eb9d2dabb98c2dc50aef3f4a3fc8fbc98f
Sharanramdb <53758737+Sharanramdb@users.noreply.github.com>
2020-11-17 15:35:25 +0530

    Create args.sh

---
+++ LAB TEST-1  17-11-20/args.sh
@@ -0,0 +1,9 @@
+#SharanRamDB - 1BM18CS097
+#Batch - C1
+
+if [ $# -ne 3 ]; then
+  echo "Please Enter 3 Arguments"
+else
+  echo "The Arguments entered are : $1 , $2 & $3"
+  echo "The total number of Arguments are : $#"
+fi
